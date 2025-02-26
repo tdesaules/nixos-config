48d92e60473e2db05dcca0140fcf0ea38fc52a50
Thibault DESAULES <thibault@desaules.me>
2025-02-26 19:21:16 +0100

    feat: Enable experimental features

--- hosts/host-1c79a46e-73a7-7849-a6e3-82d1da5bca2a/configuration.nix
+++ hosts/host-1c79a46e-73a7-7849-a6e3-82d1da5bca2a/configuration.nix
@@ -10,6 +10,10 @@ in
     hostname.hostname
     homeManagerConfig
   ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
   environment.systemPackages = with pkgs; [
     # Add other specific packages here
   ];
